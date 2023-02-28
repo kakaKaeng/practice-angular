876207adb6c55e79920aa35830f69ec81d3a558c
kakakaeng <kakakaeng@gmail.com>
2023-02-28 23:12:57 +0700

    feat(tax-input-detail): tax computation ui

--- practice-angular/src/app/modules/taxes/shared/components/tax-input-detail/tax-input-detail.component.ts
+++ practice-angular/src/app/modules/taxes/shared/components/tax-input-detail/tax-input-detail.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tax-input-detail',
   templateUrl: './tax-input-detail.component.html',
   styleUrls: ['./tax-input-detail.component.scss']
 })
-export class TaxInputDetailComponent implements OnInit {
+export class TaxInputDetailComponent  {
+  @Input() formTaxModel!: FormGroup
+  @Output() formTaxModelChange = new EventEmitter<FormGroup>();
+
+  mockTypeList = [
+    {
+      label: 'On-Time',
+      value: 'on_time'
+    }
+  ]
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
