58513bd328add849a471b0dd60ceb0a87c720adb
kakakaeng <kakakaeng@gmail.com>
2023-02-28 18:12:47 +0700

    feat(taxes-routing): lazy load module and map path to tax filling

--- practice-angular/src/app/app-routing.module.ts
+++ practice-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/taxes/taxes.module').then(m => m.TaxesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- practice-angular/src/app/modules/taxes/taxes-routing.module.ts
+++ practice-angular/src/app/modules/taxes/taxes-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaxFillingComponent } from './pages/tax-filling/tax-filling.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TaxFillingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
